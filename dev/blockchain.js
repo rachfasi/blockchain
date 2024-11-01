1be6bad08bb5f9c5af6d0261ae8a01e1d2311544
Fasihi <Richard.Fasihi@treehousefoods.com>
2024-11-01 14:46:29 -0500

    progress

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,19 +1,23 @@
+
+import sha256 from 'sha256'
+
 class Blockchain {
     constructor() {
         this.chain = []
-        this.newTransactions = []
+        this.pendingTransactions = []
+        this.createNewBlock(100, '0', '0')
     }
     createNewBlock(nonce, previousBlockHash, hash) {
         const newBlock = {
             index: this.chain.length + 1,
             timestamp: Date.now(),
-            transaction: this.newTransactions,
+            transaction: this.pendingTransactions,
             nonce: nonce,
             hash: hash,
             previousBlockHash: previousBlockHash
         }
 
-        this.newTransactions = []
+        this.pendingTransactions = []
         this.chain.push(newBlock)
 
         return newBlock
@@ -28,7 +32,25 @@ class Blockchain {
             sender: sender,
             recipient: recipient
         }
-        this.newTransactions.push(newTransaction)
+        this.pendingTransactions.push(newTransaction)
+       
+        return this.getLastBlock()['index'] + 1
+    }
+    hashBlock(previousBlockHash, currentBlockdata, nonce){
+        const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockdata)
+        const hash = sha256(dataAsString)
+        return hash
+    }
+    proofOfWork(previousBlockHash, currentBlockdata){
+        let nonce = 0
+        let hash = this.hashBlock(previousBlockHash, currentBlockdata, nonce)
+
+        while (hash.substring(0, 4) !== '0000'){
+            nonce++
+            hash = this.hashBlock(previousBlockHash, currentBlockdata, nonce)
+            console.log(hash)
+        }
+        return nonce
     }
 }
 
--- dev/test.js
+++ dev/test.js
@@ -1,11 +1,51 @@
 import Blockchain from "./blockchain.js"
-
-
 const bitcoin = new Blockchain()
-bitcoin.createNewBlock(24516, 'bcab3c9cce4df85f6772e4f0bdf1', 'bcab3c9352056c868077afea1b4c58c85')
-bitcoin.createNewBlock(24510, 'bcab3c9cce4df2352056c868077af','bcab3c9cce4df85f677c58c85')
-bitcoin.createNewBlock(24517, 'bcab3c9cce4df85f6772e5', 'bcab3c9cce4df885')
-
-bitcoin.getLastBlock()
 
 console.log(bitcoin)
+
+// const previousBlockHash = 'ASNSJSUNDHDHGDTTDYSM'
+// const currentBlockdata = [
+//     {
+//         amount: 2000,
+//         sender: 'ALEXHOAYSHSNU',
+//         recipient: 'JENNSHNJHUTSGR'
+//       },
+//       {
+//         amount: 3000,
+//         sender: 'MAMAHUYANAHATAFG',
+//         recipient: 'PAPAMKSJYNTR'
+//       },
+//       {
+//         amount: 4000,
+//         sender: 'SISNAJANASSIUYTH',
+//         recipient: 'BROSNHAHAUYHT'
+//       }
+// ]
+
+
+//console.log(bitcoin.proofOfWork(previousBlockHash, currentBlockdata))
+//0000c50a94a3f49c66b2fe0ddf47357132683d7c450bb3c8f615dc5332971e37
+//console.log(bitcoin.hashBlock(previousBlockHash, currentBlockdata, 23437 ))
+
+
+
+
+
+
+
+
+// bitcoin.createNewBlock(24516, 'bcab3c9cce4df85f6772e4f0bdf1', 'bcab3c9352056c868077afea1b4c58c85')
+
+// bitcoin.createNewTransaction(100, 'ALEXHOAYSHSNU', 'JENNSHNJHUTSGR')
+
+// bitcoin.createNewBlock(24510, 'bcab3c9cce4df2352056c868077af','bcab3c9cce4df85f677c58c85')
+
+// bitcoin.createNewTransaction(50, 'ALEXHOAYSHSNU', 'JENNSHNJHUTSGR')
+// bitcoin.createNewTransaction(300, 'ALEXHOAYSHSNU', 'JENNSHNJHUTSGR')
+// bitcoin.createNewTransaction(2000, 'ALEXHOAYSHSNU', 'JENNSHNJHUTSGR')
+
+// bitcoin.createNewBlock(24517, 'bcab3c9cce4df85f6772e5', 'bcab3c9cce4df885')
+
+// bitcoin.getLastBlock()
+
+// console.log(bitcoin.chain[2])
